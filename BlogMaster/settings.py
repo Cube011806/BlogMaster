431289ba1ef892cbb0df6ba892ca1c99a11580ea
Rafał Wronowski <raf.wro42@gmail.com>
2025-10-27 20:34:16 +0100

    DOdanie strony głównej, która jeszcze później zostanie rozbudowana

--- BlogMaster/settings.py
+++ BlogMaster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&nh!e&i(!d_9$^x@ufatd7+n$(r_$ekp7+05ekix#ilcd((hnq'
+SECRET_KEY = 'django-insecure-(09355$&ob^jvx-nx!y4ixu3sdd2%(%fv88u9q*c)18m)_9ak2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
