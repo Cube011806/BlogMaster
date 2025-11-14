81876f746d7514478455c3c0cd059d982fd19633
Rafał Wronowski <raf.wro42@gmail.com>
2025-11-14 12:27:19 +0100

    Jakieś zmiany

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
+# Create your models here.
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
@@ -14,17 +16,26 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser musi mieć is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser musi mieć is_superuser=True.")
+
         return self.create_user(email, password, **extra_fields)
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True, verbose_name="Adres e-mail")
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = CustomUserManager()
-
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = []  # nie wymagamy nic więcej przy tworzeniu superusera
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
