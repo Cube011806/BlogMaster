b4f26ab0694ab752ce31e09853cda9b10ba9aa83
Cube011806 <cube011806@gmail.com>
2026-01-19 20:58:39 +0100

    Zmieniono stronę główną

--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from posts.models import Post
+import random
 
-# Create your views here.
 def home(request):
-    return render(request, 'main/index.html')
+    # Pobieramy najnowsze posty z publicznych blogów
+    posts = Post.objects.filter(
+        blog__is_public=True
+    ).order_by('-created_at')[:20]  # np. 20 najnowszych
 
+    # Losujemy kolejność
+    posts = list(posts)
+    random.shuffle(posts)
+
+    # Ograniczamy do np. 6 wyświetlanych
+    posts = posts[:6]
+
+    return render(request, 'main/index.html', {
+        'posts': posts
+    })
